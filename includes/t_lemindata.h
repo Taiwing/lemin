8243d9a4938bcbb9668141ac950ddd7ae585872d
Tristan PONESSA <trponess@e1r12p7.42.fr>
2019-11-22 17:03:55 +0100

    dict works + test function

--- includes/t_lemindata.h
+++ includes/t_lemindata.h
@@ -6,7 +6,7 @@
 /*   By: trponess <trponess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 16:36:53 by yforeau           #+#    #+#             */
-/*   Updated: 2019/11/22 11:13:28 by trponess         ###   ########.fr       */
+/*   Updated: 2019/11/22 15:37:29 by trponess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 # define T_LEMINDATA_H
 # include "t_vertex.h"
 
-typedef struct		s_dict
+#define HASH_SIZE 4096
+typedef t_list *t_dict[HASH_SIZE];
+
+//t_dict mydict;
+//t_list *mydict[HASH_SIZE];
+
+typedef struct		s_dict_elem
 {
 	const char		*key;
-	t_list			*val;//linkedlist of vertexes
-}					t_dict;
+	const void			*val;//linkedlist of vertexes
+}					t_dict_elem;
 
 typedef struct	s_lemindata
 {
@@ -33,7 +39,7 @@ typedef struct	s_lemindata
 	int			**e;
 	int			s;
 	int			t;
-	t_dict		v_dict[10000];
+	t_dict		v_dict;
 }				t_lemindata;
 
 #endif
--- src/parser/parser_stock.c
+++ src/parser/parser_stock.c
@@ -6,7 +6,7 @@
 /*   By: trponess <trponess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 14:37:38 by trponess          #+#    #+#             */
-/*   Updated: 2019/11/22 11:51:14 by trponess         ###   ########.fr       */
+/*   Updated: 2019/11/22 17:02:22 by trponess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,107 +24,158 @@ unsigned long	hash_djb2(void *ptr)
 	hash = 5381;
 	while ((c = *str++))
 		hash += (hash << 5) + c;
-	return (hash % 10000);
+	return (hash % HASH_SIZE);
 }
 
 static void init_vertex(t_lemindata *lda)
 {
-    int i = 0;
+	int i = 0;
 
-    lda->v = (t_vertex **)ft_secmalloc(sizeof(t_vertex *) * (lda->nb_rooms));
-    //lda->v = (t_vertex **)malloc(sizeof(t_vertex *) * (lda->nb_rooms));
-    while (i < lda->nb_rooms)
-    {
-        lda->v[i] = (t_vertex *)ft_secmalloc(sizeof(t_vertex));    
-        //lda->v[i] = (t_vertex *)malloc(sizeof(t_vertex));
-        lda->v[i]->id = -1;
-        ++i;
-    }
+	lda->v = (t_vertex **)ft_secmalloc(sizeof(t_vertex *) * (lda->nb_rooms));
+	//lda->v = (t_vertex **)malloc(sizeof(t_vertex *) * (lda->nb_rooms));
+	while (i < lda->nb_rooms)
+	{
+		lda->v[i] = (t_vertex *)ft_secmalloc(sizeof(t_vertex));    
+		//lda->v[i] = (t_vertex *)malloc(sizeof(t_vertex));
+		lda->v[i]->id = -1;
+		++i;
+	}
 }
-/*
-void stock_v_in_dict(t_lemindata *lda, int map_size)
+
+void stock_v_in_dict(t_lemindata *lda)
 {
-    int i = 0;
+	int i = 0;
 
-    while (lda->v[i])
-    {
-        char *room_name = ft_strdup(lda->v[i]->name);
-        lda->v_dict[hash_djb2(room_name)].key = ft_strdup(room_name);
-        lda->v_dict[hash_djb2(room_name)].val = ft_lstnew(NULL, 0);
-        ft_lst_push_front(lda->v_dict[hash_djb2(room_name)].val, lda->v[i],0);
-        ++i;
-    }
+	ft_bzero(lda->v_dict, sizeof(t_dict));
+	while (i < lda->nb_rooms)
+	{
+		int key_nb = hash_djb2(lda->v[i]->name);
+		
+		t_dict_elem d;
+		d.key = lda->v[i]->name;
+		d.val = lda->v[i];
+		ft_lst_push_back(&lda->v_dict[key_nb], &d,sizeof(t_dict_elem));
+		
+//-----------TEST-----------------
+/*int nb_elem = 0;
+int max = ft_lst_size(lda->v_dict[key_nb]);
+ft_printf("-----v_dict[%d]---------\n", key_nb);
+while (nb_elem < max)
+{
+	t_dict_elem *x = (t_dict_elem*)ft_lst_at(lda->v_dict[key_nb], nb_elem)->content;
+	const char *k = x->key;
+	t_vertex *v = (t_vertex *)x->val;
+	ft_printf("content of lda->v_dict[%d(%s)] elem %d : key <%s> val-vertex:[roomname]<%s> [id]<%d> [status]<%d>\n", \
+			key_nb,k, nb_elem,\
+			k, v->name, v->id, v->status); 
+	nb_elem++;
+	
 }
-*/
+ft_printf("-----DONE-----------\n");*/
+//------------------------------
+		++i;
+	}
+}
+
+void DISPLAY_DICTIONARY(t_lemindata *lda)//need nb_rooms, dict
+{
+	int i = 0;
+
+	while (i < lda->nb_rooms)
+	{
+		int key_nb = hash_djb2(lda->v[i]->name);
+		int nb_elem = 0;
+		int max = ft_lst_size(lda->v_dict[key_nb]);
+		ft_printf("-----v_dict[%d] for vertex room %s---------\n", key_nb, lda->v[i]->name);
+		while (nb_elem < max)
+		{
+			t_dict_elem *x = (t_dict_elem*)ft_lst_at(lda->v_dict[key_nb], nb_elem)->content;
+			const char *k = x->key;
+			t_vertex *v = (t_vertex *)x->val;
+			ft_printf("content of lda->v_dict[%d(%s)] elem %d : key <%s> val-vertex:[roomname]<%s> [id]<%d> [status]<%d>\n", \
+					key_nb,k, nb_elem,\
+					k, v->name, v->id, v->status); 
+			nb_elem++;
+			
+		}
+		ft_printf("-----DONE-----------\n");
+		++i;
+	}
+}
+
+/*
+void init_adj_list(t_lemindata *lda)
+{
+	int i = 0;
+
+	ft_bzero(lda->v_dict, sizeof(t_dict));
+	while (i < lda->nb_rooms)
+	{
+
+	}
+	
+}*/
+
 void parser_stock(t_lemindata *lda)
 {
-    lda->antn = (long int)ft_atoi(ft_lst_at(lda->map, 0)->content);
-    
-    lda->s = 0;
+	lda->antn = (long int)ft_atoi(ft_lst_at(lda->map, 0)->content);	
+	lda->s = 0;
 
-    int i = 1;
-    int vi = 0;
-    int isstart = 0, isend = 0;
+	int i = 1;
+	int vi = 0;
+	int isstart = 0, isend = 0;
 
-    init_vertex(lda);
-    int size_map = ft_lst_size(lda->map);
-    //t_vertex *v = (t_vertex *)malloc(sizeof(t_vertex) * map_size);//i dont know number of rooms
-    while (i < size_map)
-    {
-        isstart = 0;
-        isend = 0;
-        char *line = (char*)ft_lst_at(lda->map, i)->content;
+	init_vertex(lda);
+	int size_map = ft_lst_size(lda->map);
+	//t_vertex *v = (t_vertex *)malloc(sizeof(t_vertex) * map_size);//i dont know number of rooms
+	while (i < size_map)
+	{
+		isstart = 0;
+		isend = 0;
+		char *line = (char*)ft_lst_at(lda->map, i)->content;
 
-        if (!ft_strcmp("##start", line))
-        {
-            ++i;
-            line = (char*)ft_lst_at(lda->map, i)->content;
-            isstart = 1;
-        }
-        if (!ft_strcmp("##end", line))
-        {
-            ++i;
-            line = (char*)ft_lst_at(lda->map, i)->content;
-            isend = 1;
-        }
-        if (is_room(line))
-        {
-            char *room_name = ft_split_whitespaces(line)[0];//watch out for leaks
+		if (!ft_strcmp("##start", line))
+		{
+			++i;
+			line = (char*)ft_lst_at(lda->map, i)->content;
+			isstart = 1;
+		}
+		if (!ft_strcmp("##end", line))
+		{
+			++i;
+			line = (char*)ft_lst_at(lda->map, i)->content;
+			isend = 1;
+		}
+		if (is_room(line))
+		{
+			char *room_name = ft_split_whitespaces(line)[0];//watch out for leaks
 
-            lda->v[vi]->name = ft_strdup(room_name);
-            lda->v[vi]->id = vi;
-            lda->v[vi]->status = NORMAL;
-            if (isstart)
-                lda->v[vi]->status = SOURCE;
-            if (isend)
-                lda->v[vi]->status = SINK;
+			lda->v[vi]->name = ft_strdup(room_name);
+			lda->v[vi]->id = vi;
+			lda->v[vi]->status = NORMAL;
+			if (isstart)
+				lda->v[vi]->status = SOURCE;
+			if (isend)
+				lda->v[vi]->status = SINK;
 
-            //lda->v[vi]->status = (isstart == 1) ? SOURCE : NORMAL;
-           // lda->v[vi]->status = (isend) ? SINK : NORMAL;
 
-            ++vi;
-            //do adj after with links
+			//lda->v[vi]->status = (isstart == 1) ? SOURCE : NORMAL;
+		   // lda->v[vi]->status = (isend) ? SINK : NORMAL;
 
-        }
-        if (is_link(line))
-            break;
-        ++i;
-    }
+			++vi;
+			//do adj after with links
 
-    /*
-    init_vertex(lda);
-    int i = 0;int vi = 0;
-    while (i < map_size)
-    {
-        while (i < map_size)
-        {
-            t_list *n = ft_lst_at(lda->map, i);
-            lda-> = n->content
-            ++vi;
-        }
-        vi = 0;
-        ++i;
-    }*/
+		}
+		if (is_link(line))
+			break;
+		++i;
+	}
+
+	stock_v_in_dict(lda);
+
+	DISPLAY_DICTIONARY(lda);
+
+	
 
 
 
