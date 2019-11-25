d96294ed0fadcb22fdf543df601dbc27b5858f68
Tristan PONESSA <trponess@e1r12p7.42.fr>
2019-11-25 16:40:39 +0100

    last seg adj and matrix works on small map

--- src/parser/parser_stock.c
+++ src/parser/parser_stock.c
@@ -6,7 +6,7 @@
 /*   By: trponess <trponess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 14:37:38 by trponess          #+#    #+#             */
-/*   Updated: 2019/11/25 15:04:07 by trponess         ###   ########.fr       */
+/*   Updated: 2019/11/25 16:22:36 by trponess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ static void init_vertex(t_lemindata *lda)
 {
 	int i = 0;
 
-	lda->v = (t_vertex **)ft_secmalloc(sizeof(t_vertex *) * (lda->nb_rooms));
+	lda->v = (t_vertex **)ft_memalloc(sizeof(t_vertex *) * (lda->nb_rooms));
 	//lda->v = (t_vertex **)malloc(sizeof(t_vertex *) * (lda->nb_rooms));
 	while (i < lda->nb_rooms)
 	{
-		lda->v[i] = (t_vertex *)ft_secmalloc(sizeof(t_vertex));    
+		lda->v[i] = (t_vertex *)ft_memalloc(sizeof(t_vertex));    
 		//lda->v[i] = (t_vertex *)malloc(sizeof(t_vertex));
 		lda->v[i]->id = -1;
 		++i;
@@ -55,6 +55,7 @@ void stock_v_in_dict(t_lemindata *lda)
 		d.key = lda->v[i]->name;
 		d.val = lda->v[i];
 		ft_lst_push_back(&lda->v_dict[key_nb], &d,sizeof(t_dict_elem));
+		//ft_lstadd(&lda->v[i]->adj, ft_lstnew(NULL, 0));//causes 
 		++i;
 	}
 }
@@ -144,6 +145,7 @@ void DISPLAY_ADJ_TABLE(t_lemindata *lda)
 		j = 0;
 		++i;
 	}
+	ft_printf("\n");
 }
 
 t_dict_elem *search_for_dict_elem(t_lemindata *lda, char *room)
@@ -166,10 +168,10 @@ void init_edges(t_lemindata *lda)
 {
 	int i = 0;
 
-	lda->e = (int **)ft_memalloc(sizeof(int *) * (lda->nb_rooms));
+	lda->e = (int **)ft_memalloc(sizeof(int *) * (lda->nb_rooms + 10000));
 	while (i < lda->nb_rooms)
 	{
-		lda->e[i] = (int *)ft_memalloc(sizeof(int) * (lda->nb_rooms));
+		lda->e[i] = (int *)ft_memalloc(sizeof(int) * (lda->nb_rooms + 10000));
 		++i;
 	}	
 }
@@ -197,15 +199,17 @@ t_vertex *get_vertex_from_dict(t_lemindata *lda, char *room)
 
 void stock_adjlist_and_e(t_lemindata *lda)
 {
+	if (!lda)
+		return ;
 	int i = 0;
-	//ft_bzero(lda->v_dict, sizeof(t_dict));
 	int size_map = ft_lst_size(lda->map);
+	
 	//t_vertex *v = (t_vertex *)malloc(sizeof(t_vertex) * map_size);//i dont know number of rooms
 	while (i < size_map)
 	{
 		char *line = (char*)ft_lst_at(lda->map, i)->content;
 
-		if (is_link(line))
+		if (line && is_link(line))
 		{
 			
 			char **rooms = ft_strsplit(line, '-');//tofree
@@ -213,29 +217,53 @@ void stock_adjlist_and_e(t_lemindata *lda)
 			
 			t_vertex *v1 = get_vertex_from_dict(lda, rooms[0]);
 			t_vertex *v2 = get_vertex_from_dict(lda, rooms[1]);
-			if (!v1 || !v2)
-				return ;
+			//if (!v1 || !v2 || !v1->adj || !v2->adj)
+			//	return ;
 			//v1->adj = ft_lstnew(NULL, 0);
+			if (!v1)
+				ft_printf("NOOOOO V1\n");
+			if (!v2)
+				ft_printf("NOOOOO V2\n");
+			ft_printf("PLOP\n");
 			t_list *x = ft_lst_push_back(&v1->adj, NULL, 0);
 			t_list *y = ft_lst_push_back(&v2->adj, NULL, 0);
+			ft_printf("PLOP2\n");
+			if (!x || !y)
+			{
+				ft_printf("fucking X Y is EMPTY");
+				return ;
+			}
 			//x->content = (int *)ft_memalloc(sizeof(int));
 			//(int *)x->content[0] = v2->id;
 
 		//!!if someone writes r1-r2 and r2-r1 it will be added twice everywhere
 			x->content = &(v2->id);
 			void *z = x->content;
+			if (!z)
+			{
+				ft_printf("fucking z is EMPTY");
+				return ;
+			}
 			ft_printf("adjecing adding %d\n", *(int *)z);
 			y->content = &(v1->id);
 			void *w = y->content;
+			if (!w)
+			{
+				ft_printf("fucking w is EMPTY");
+				return ;
+			}
 			ft_printf("adjecing adding %d\n", *(int *)w);
 
 
 			//e 
 			ft_printf("connecting %d %d\n", v1->id, v2->id);
+			
 			lda->e[v1->id][v2->id] = 1;
 			lda->e[v2->id][v1->id] = 1;
 
-			if (!v1->adj->content)
+			ft_printf("e success\n");
+
+			if (!v1->adj)
 				ft_printf("IS FUCKING EMPTY!!!\n");
 			//else	
 				//ft_printf("EXSIST>? %d -> \n", *(int *)(v1->adj->content));
@@ -286,9 +314,15 @@ void parser_stock(t_lemindata *lda)
 			lda->v[vi]->id = vi;
 			lda->v[vi]->status = NORMAL;
 			if (isstart)
+			{
+				lda->s = vi;
 				lda->v[vi]->status = SOURCE;
+			}
 			if (isend)
+			{
+				lda->t = vi;
 				lda->v[vi]->status = SINK;
+			}
 
 
 			//lda->v[vi]->status = (isstart == 1) ? SOURCE : NORMAL;
@@ -309,13 +343,13 @@ void parser_stock(t_lemindata *lda)
 	//get id id from v_dict[853(Oyt4)]
 	//get_id from v_dict[853(K_u1)]
 
-	t_vertex *a = get_vertex_from_dict(lda, "Asa5");//start 
-	t_vertex *b = get_vertex_from_dict(lda, "Cfl8");//end
+	//t_vertex *a = get_vertex_from_dict(lda, "Asa5");//start 
+	//t_vertex *b = get_vertex_from_dict(lda, "Cfl8");//end
 	DISPLAY_EDGED_NATRIX(lda);
 	DISPLAY_ADJ_TABLE(lda);
 
 	
 	
-	(void)a;
-	(void)b;
+	//(void)a;
+	//(void)b;
 }
