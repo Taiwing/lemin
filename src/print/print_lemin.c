daaaadb3185b316e599defcf4263b17ccb7eefeb
Yoann FOREAU <yforeau@e3r10p7.42.fr>
2019-11-27 22:52:49 +0100

    Print lemin solution better
    There still are problems though

--- src/print/print_lemin.c
+++ src/print/print_lemin.c
@@ -6,14 +6,14 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 14:54:20 by yforeau           #+#    #+#             */
-/*   Updated: 2019/11/27 20:45:22 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/11/27 22:40:26 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin_options.h"
 #include "print_lemin.h"
 
-static int	move_ants(t_ant *ant, t_list *solution, int len)
+static int	move_ants(t_ant *ant, t_list *solution, int len, int *moving)
 {
 	int	i;
 	int	arrived;
@@ -24,9 +24,10 @@ static int	move_ants(t_ant *ant, t_list *solution, int len)
 	{
 		if (ant[i].state == INACTIVE && solution)
 		{
-			ant[i].room = ((t_leminpath *)solution->content)->nodes;
+			ant[i].room = ((t_leminpath *)solution->content)->nodes->next;
 			solution = solution->next;
 			ant[i].state = MOVING;
+			++(*moving);
 		}
 		else if (ant[i].state == MOVING)
 		{
@@ -35,6 +36,7 @@ static int	move_ants(t_ant *ant, t_list *solution, int len)
 				ant[i].state = ARRIVED;
 				++arrived;
 			}
+			*moving = ant[i].state == MOVING ? *moving + 1 : *moving;
 		}
 	}
 	return (arrived);
@@ -43,6 +45,7 @@ static int	move_ants(t_ant *ant, t_list *solution, int len)
 static void	print_solution(t_lemindata *lda, t_list *solution)
 {
 	t_ant	*ant;
+	int		moving;
 	int		n;
 	int		i;
 	
@@ -50,14 +53,15 @@ static void	print_solution(t_lemindata *lda, t_list *solution)
 	ant = ft_memalloc(n * sizeof(t_ant));
 	while (n)
 	{
-		n -= move_ants(ant, solution, lda->antn);
+		moving = 0;
+		n -= move_ants(ant, solution, lda->antn, &moving);
 		i = -1;
 		while (n && ++i < lda->antn)
 		{
 			if (ant[i].state == MOVING)
 				ft_printf("L%d-%s%c", i + 1,
 					lda->v[*(int *)ant[i].room->content]->name,
-					i < lda->antn - 1 ? ' ' : '\n');
+					--moving ? ' ' : '\n');
 		}
 	}
 }
