42c780cfd6e3e36acddf5110816ccd2e9d9ce4f1
Tristan PONESSA <trponess@e1r1p7.42.fr>
2019-11-27 13:55:13 +0100

    all works test other maps

--- src/parser/lemin_parser.c
+++ src/parser/lemin_parser.c
@@ -6,7 +6,7 @@
 /*   By: trponess <trponess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 14:37:38 by trponess          #+#    #+#             */
-/*   Updated: 2019/11/26 18:59:43 by trponess         ###   ########.fr       */
+/*   Updated: 2019/11/27 13:54:56 by trponess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,9 @@ unsigned long	hash_djb2(void *ptr)
 
 int is_num(const char *str)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	if (!str)
 		return (0);
 	if (str[0] == '-' || str[0] != '+')
@@ -46,26 +47,37 @@ int is_num(const char *str)
 }
 
 
-int is_room(const char *line)
+int is_room_link(const char *line, char option)
 {
-	//roomn N N\n
+	char **p;
+	
 	if (!line)
 		return (0);
-	char **p = ft_strsplit(line, ' ');//dont forget to free
-	if (ft_wtlen(p) == 3)
+	if (option == 'r')
+		p = ft_strsplit(line, ' ');
+	else
+		p = ft_strsplit(line, '-');
+	if (option == 'r')
 	{
-		if (ft_strlen(p[0]) > 0 && is_num(p[1]) && is_num(p[2]))
+		if (ft_wtlen(p) == 3 && ft_strlen(p[0]) > 0 && is_num(p[1]) && is_num(p[2]))
 		{
 			ft_printf("IS ROOM <%s>\n", line);
 			return (1);
 		}
 	}
+	else if (ft_wtlen(p) == 2 && !ft_strstr(line, " ") && \
+			ft_strlen(p[0]) > 0 && ft_strlen(p[1]) > 0)
+	{
+		ft_printf("IS LINK <%s>\n", line);
+		return (1);
+	}
 	return (0);
 }
-
+/*
 int is_link(const char *line)
 {
-	//link N-N\n
+	char **p
+
 	if (!line)
 		return (0);
 	if (ft_strstr(line, " "))
@@ -81,9 +93,7 @@ int is_link(const char *line)
 	}
 	return (0);
 }
-
-
-
+*/
 
 void DISPLAY_DICTIONARY(t_lemindata *lda)//need nb_rooms, dict
 {
@@ -102,7 +112,7 @@ void DISPLAY_DICTIONARY(t_lemindata *lda)//need nb_rooms, dict
 			t_vertex *v = (t_vertex *)x->val;
 			ft_printf("content of lda->v_dict[%d(%s)] elem %d : key <%s> val-vertex:[roomname]<%s> [id]<%d> [status]<%d>\n", \
 					key_nb,k, nb_elem,\
-					k, v->name, v->id, v->status); 
+					k, v->name, v->id, v->status);
 			nb_elem++;
 			
 		}
@@ -190,122 +200,58 @@ void DISPLAY_VERTEX(t_lemindata *lda)
 	ft_printf("\n");
 }
 
-t_dict_elem *search_for_dict_elem(t_lemindata *lda, char *room)
-{
-		int key_nb = hash_djb2(room);
-		int nb_elem = 0;
-		int max = ft_lst_size(lda->v_dict[key_nb]);
-		while (nb_elem < max)
-		{
-			t_dict_elem *x = (t_dict_elem*)ft_lst_at(lda->v_dict[key_nb], nb_elem)->content;
-			const char *k = x->key;
-			if (!ft_strcmp(room, k))
-				return (x);
-			nb_elem++;
-		}
-		return (NULL);
-}
-
-
-
 t_vertex *get_vertex_from_dict(t_lemindata *lda, char *room)
 {
-	ft_printf("getting dict_elem_value vertex in dict name <%s> ....\n", room);
-	t_list *elem_list = (t_list *)lda->v_dict[hash_djb2(room)];
-	if (!elem_list)
+	int			key_nb;
+	int			max;
+	int			nb_elem;
+	t_dict_elem	*x;
+	
+	key_nb = hash_djb2(room);
+	nb_elem = 0;
+	max = ft_lst_size(lda->v_dict[key_nb]);
+	while (nb_elem < max)
 	{
-		ft_printf("hash room not in dict\n");
-		return (0);
+		x = (t_dict_elem*)ft_lst_at(lda->v_dict[key_nb], nb_elem)->content;
+		if (!ft_strcmp(room, x->key))
+			return ((t_vertex *)x->val);
+		nb_elem++;
 	}
-	t_dict_elem *d = search_for_dict_elem(lda, room); 
-	if (!d)
-	{
-		ft_printf("FOUND EXCEPTION>>>>>>>>>>>>the key wasnt found dict\n");
-		return (0);
-	}
-	t_vertex *z = (t_vertex*)d->val;
-	ft_printf("			!!!found name:%s id:%d status:%d\n",z->name, z->id, z->status); 
-	return ((t_vertex *)d->val);
+	return (NULL);
 }
-
 
 
 void stock_adjlist_and_e(t_lemindata *lda)
 {
-	if (!lda)
-		return ;
-	int i = 0;
-	int size_map = ft_lst_size(lda->map);
-	
-	//t_vertex *v = (t_vertex *)malloc(sizeof(t_vertex) * map_size);//i dont know number of rooms
-	while (i < size_map)
+	int		i;
+	char	*line;
+	char	**rooms;
+	t_vertex *v[2];
+	t_list	*adj[2];
+
+	i = 0;
+	while (i < lda->map_size)
 	{
-		char *line = (char*)ft_lst_at(lda->map, i)->content;
-
-		if (line && is_link(line))
+		line = (char*)ft_lst_at(lda->map, i)->content;
+		if (is_room_link(line, 'l'))
 		{
-			
-			char **rooms = ft_strsplit(line, '-');//tofree
-			ft_printf("line %s is link -> cutting r1 %s r2 %s\n", line, rooms[0], rooms[1]);
-			
-			t_vertex *v1 = get_vertex_from_dict(lda, rooms[0]);
-			t_vertex *v2 = get_vertex_from_dict(lda, rooms[1]);
-			//if (!v1 || !v2 || !v1->adj || !v2->adj)
-			//	return ;
-			//v1->adj = ft_lstnew(NULL, 0);
-			if (!v1)
-				ft_printf("NOOOOO V1\n");
-			if (!v2)
-				ft_printf("NOOOOO V2\n");
-			
-			t_list *x = ft_lst_push_back(&v1->adj, NULL, 0);
-			t_list *y = ft_lst_push_back(&v2->adj, NULL, 0);
-			
-			if (!x || !y)
-			{
-				ft_printf("fucking X Y is EMPTY");
-				return ;
-			}
-			//x->content = (int *)ft_memalloc(sizeof(int));
-			//(int *)x->content[0] = v2->id;
-
-		//!!if someone writes r1-r2 and r2-r1 it will be added twice everywhere
-			x->content = &(v2->id);
-			void *z = x->content;
-			if (!z)
-			{
-				ft_printf("fucking z is EMPTY");
-				return ;
-			}
-			ft_printf("adjecing adding %d\n", *(int *)z);
-			y->content = &(v1->id);
-			void *w = y->content;
-			if (!w)
-			{
-				ft_printf("fucking w is EMPTY");
-				return ;
-			}
-			ft_printf("adjecing adding %d\n", *(int *)w);
-
-
-			//e 
-			ft_printf("connecting %d %d\n", v1->id, v2->id);
-			
-			lda->e[v1->id][v2->id] = 1;
-			lda->e[v2->id][v1->id] = 1;
-
-			if (!v1->adj)
-				ft_printf("IS FUCKING EMPTY!!!\n");
-			//else	
-				//ft_printf("EXSIST>? %d -> \n", *(int *)(v1->adj->content));
-
-			ft_printf("\n");
+			rooms = ft_strsplit(line, '-');//tofree
+				//ft_printf("line %s is link -> cutting r1 %s r2 %s\n", line, rooms[0], rooms[1]);
+			v[0] = get_vertex_from_dict(lda, rooms[0]);
+			v[1] = get_vertex_from_dict(lda, rooms[1]);
+			adj[0] = ft_lst_push_back(&v[0]->adj, NULL, 0);
+			adj[1] = ft_lst_push_back(&v[1]->adj, NULL, 0);
+			adj[0]->content = &(v[1]->id);
+			adj[1]->content = &(v[0]->id);
+				//ft_printf("adjecing adding %d\n", *(int *)adj[0]->content);
+				//ft_printf("adjecing adding %d\n", *(int *)adj[1]->content);
+				//ft_printf("connecting %d %d\n", v[0]->id, v[1]->id);
+			lda->e[v[0]->id][v[1]->id] = 1;
+			lda->e[v[1]->id][v[0]->id] = 1;
 		}
 		++i;
 	}
-	
 }
-
 
 void stock_map(t_lemindata *lda)
 {
@@ -324,74 +270,6 @@ void stock_map(t_lemindata *lda)
 	}
 	lda->map_size = map_size;
 }
-
-
-/*
-int is_start(char *line, int *i)
-{
-	if (!ft_strcmp("##start", line))
-		{
-			++i;
-			line = (char*)ft_lst_at(lda->map, i)->content;
-			isstart = 1;
-		}
-		if (!ft_strcmp("##end", line))
-		{
-			++i;
-			line = (char*)ft_lst_at(lda->map, i)->content;
-			isend = 1;
-		}
-}
-
-void is_start_end(t_lemindata *lda, char *line, int *i, int *startend)
-{
-	int start;
-	int end;
-	
-
-	start = ft_strcmp("##start", line);
-	end = ft_strcmp("##end", line);
-
-	if (start != 0 || end != 0)
-		++(*i);
-	if (start != 0)
-	{
-		*startend = 1;
-
-	}
-	else if (end != 0)
-		*startend = 2;
-	else 
-		*startend = 0;
-	
-}
-*/
-
-/*
-int		start_end_line(t_lemindata *lda, char o)
-{
-	static int	start_l;
-	static int	end_l;
-	int			i;
-	char		*line;
-
-	i = 0;
-	if (!start_l)
-	{
-		while (i < lda->map_size)
-		{
-			line = (char*)ft_lst_at(lda->map, i)->content;
-			if (!ft_strcmp("##start", line))
-				start_l = i + 1;
-			if (!ft_strcmp("##end", line))
-				end_l = i + 1;
-			if (start_l > 0 && end_l > 0)
-				break;
-			++i;
-		}
-	}
-	return (o == 's' ? start_l : end_l);
-}*/
 
 
 void init_vertex_edges(t_lemindata *lda)
@@ -441,7 +319,7 @@ void room_to_vertex(t_lemindata *lda)
 	while (li < lda->map_size)
 	{
 		line = (char*)ft_lst_at(lda->map, li)->content;
-		if (is_room(line))
+		if (is_room_link(line, 'r'))
 		{
 			room_name = ft_split_whitespaces(line)[0];
 			lda->v[vi]->name = room_name;
@@ -479,6 +357,7 @@ void lemin_parser(t_lemindata *lda)
 	init_vertex_edges(lda);
 	room_to_vertex(lda);
 	stock_vertex_lst_in_dict(lda);
+	stock_adjlist_and_e(lda);
 
 
 //	stock_v_in_dict(lda);
@@ -489,16 +368,23 @@ void lemin_parser(t_lemindata *lda)
 	//get id id from v_dict[853(Oyt4)]
 	//get_id from v_dict[853(K_u1)]
 
-	//t_vertex *a = get_vertex_from_dict(lda, "Asa5");//start 
-	//t_vertex *b = get_vertex_from_dict(lda, "Cfl8");//end
+	//maps/Sample_maps/big/generator--big_round_01
+	t_vertex *a = get_vertex_from_dict(lda, "Asa5");//start 
+	t_vertex *b = get_vertex_from_dict(lda, "Cfl8");//end
+	if (a && b)
+	{
+	ft_printf("\nvertex %c:\n[roomname]<%s>\n[id]<%d>\n[status]%d\n[adj]notset\n\n",'a', a->name, a->id, a->status);
+	ft_printf("\nvertex %c:\n[roomname]<%s>\n[id]<%d>\n[status]%d\n[adj]notset\n\n",'b', b->name, b->id, b->status);
+	}
+
 
 	//-----------test------------------------------
 	//EEXECUTE IN ORDER
-	DISPLAY_VERTEX(lda);
-	DISPLAY_DICTIONARY(lda);
-	//DISPLAY_EDGED_NATRIX(lda);
-	//DISPLAY_ADJ_TABLE(lda);
-	DISPLAY_SIZE_INFO(lda);
+	//DISPLAY_VERTEX(lda);
+	//DISPLAY_DICTIONARY(lda);
+	DISPLAY_EDGED_NATRIX(lda);
+	DISPLAY_ADJ_TABLE(lda);
+	//DISPLAY_SIZE_INFO(lda);
 	//-----------test------------------------------
 	
 	
